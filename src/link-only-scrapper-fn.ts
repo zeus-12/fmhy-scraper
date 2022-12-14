81ad0e41e65062319278b2881f563082253f3ebd
Vishnu <vishnuvinod072@gmail.com>
2022-12-14 22:32:23 +0530

    Feat: added scraping for links alone

---
+++ src/link-only-scrapper-fn.ts
@@ -0,0 +1,73 @@
+import * as cheerio from "cheerio";
+import fetch from "node-fetch";
+import { currentEle, prettifyTitle } from "../utils/helper";
+import { LinkType, CategoryType } from "../utils/types";
+
+async function get_links(
+  $: cheerio.CheerioAPI,
+  children: cheerio.Cheerio<cheerio.Element>,
+  i: number
+) {
+  let res: LinkType[] = [];
+  let nextUl = $(children[i]).next("ul");
+  let curLi = $(nextUl).children("li");
+
+  curLi.each((_, element) => {
+    let item: LinkType = { title: "", link: [], starred: false };
+
+    let title = $(element).text();
+    if (title.includes("⭐ ")) {
+      item.starred = true;
+    }
+
+    item.title = prettifyTitle($(element), "⭐ ");
+
+    // for bolded links
+    $(element)
+      .children("strong")
+      .children("a")
+      .each((_: number, element: cheerio.Element) => {
+        let link = $(element).attr("href");
+        // @ts-ignore
+        item.link.push(link);
+      });
+
+    $(element)
+      .children("a")
+      .each((_: number, element: cheerio.Element) => {
+        let link = $(element).attr("href");
+        // @ts-ignore
+        item.link.push(link);
+      });
+
+    res.push(item);
+  });
+
+  return res;
+}
+
+export default async function scrape(urlEnding: string) {
+  const html = await fetch(
+    `https://github.com/nbats/FMHYedit/blob/main/${urlEnding}.md`
+  );
+  const text = await html.text();
+
+  const $ = cheerio.load(text);
+
+  var markdown = $(".markdown-body").children();
+
+  let i = 0;
+  var res: any = [];
+
+  while (i < markdown.length) {
+    if (currentEle("h1", $(markdown[i])) || currentEle("h2", $(markdown[i]))) {
+      let temp = await get_links($, markdown, i);
+      res = res.concat(temp);
+
+      i += 1;
+    } else {
+      i += 1;
+    }
+  }
+  return res;
+}
