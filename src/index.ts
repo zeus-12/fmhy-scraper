443c6ba50a8cee90f96cddda1d798733d8c6a3eb
Vishnu <vishnuvinod072@gmail.com>
2023-05-20 10:30:35 +0530

    add write-db for base64

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-// import scrapeBase64AndAddToDb from "@/scrapers/base64";
-import scrapeAndAddToDb from "./scrapers/wiki";
+import scrapeBase64AndAddToDb from "./scrapers/base64";
+// import scrapeAndAddToDb from "./scrapers/wiki";
 
 const main = async () => {
-  await scrapeAndAddToDb();
-  // await scrapeBase64AndAddToDb();
+  // await scrapeAndAddToDb();
+  await scrapeBase64AndAddToDb();
 };
 
 main().catch((err) => console.error(err.message));
--- src/scrapers/base64/db.ts
+++ src/scrapers/base64/db.ts
@@ -1,2 +1,13 @@
-const addBase64LinksToDb = async (data: any) => {};
+import prisma from "../../utils/prisma";
+
+const addBase64LinksToDb = async (data: any) => {
+  await prisma?.base64.createMany({
+    data: data.map((item: { title: string; hash: string }) => {
+      return {
+        title: item.title,
+        hash: item.hash,
+      };
+    }),
+  });
+};
 export default addBase64LinksToDb;
