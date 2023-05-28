0c996d542b70db5e3d1acadac8831723e720d246
Vishnu <vishnuvinod072@gmail.com>
2023-05-28 20:17:27 +0530

    uncomment cause im dumb

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import scrapeBase64AndAddToDb from "./scrapers/base64";
-// import scrapeAndAddToDb from "./scrapers/wiki";
-// import cron from "node-cron";
+import scrapeAndAddToDb from "./scrapers/wiki";
+import cron from "node-cron";
 const main = async () => {
-  //
-  // await scrapeAndAddToDb();
+  await scrapeAndAddToDb();
   await scrapeBase64AndAddToDb();
 };
 
-main().catch((err) => console.error(err.message));
+// main().catch((err) => console.error(err.message));
 
-// cron.schedule("0 0 */2 * *", async () => {
-//   await main();
-// });
+cron.schedule("0 0 */2 * *", async () => {
+  await main();
+});
